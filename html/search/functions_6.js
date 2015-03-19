e13b8cd128e6f90275a1597bf88a5472527efdb2
Jan Medvesek <jan.medvesek@gmail.com>
2015-03-19 16:50:21 +0100

    Updated Protocol documentation.

--- html/search/functions_6.js
+++ html/search/functions_6.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['getbutton',['getButton',['../group___engduino_protocol.html#gad53e6d5d3c390c3eca7f0456aa7a5d05',1,'EngduinoProtocolClass']]],
+  ['getpinsvalue',['getPinsValue',['../group___engduino_protocol.html#ga6945fb3a4a3bb9b86e602168e21628b7',1,'EngduinoProtocolClass']]],
   ['getsensor',['getSensor',['../group___engduino_protocol.html#ga504071d488f9d2a9ace156c4523ea18d',1,'EngduinoProtocolClass']]],
   ['getversion',['getVersion',['../group___engduino_protocol.html#gad585cd4ca7ea413a137fe5783825a612',1,'EngduinoProtocolClass']]]
 ];
